bb8a6f2e1e19594fc3670c9775c61f94da2204d7
shajiaffan <129276229+shajiaffan@users.noreply.github.com>
2025-02-08 03:16:00 +0530

    Update apps.py

--- apps.py
+++ apps.py
@@ -61,6 +61,3 @@ def caption_image():
     except Exception as e:
         print(f"Server Error: {e}")
         return jsonify({"error": str(e)}), 500
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
